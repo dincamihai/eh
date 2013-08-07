be2582fb4a094ce1b8e5a4dcda0f385fa02ab3a2
Mihai Dinca <mihai.dinca@eaudeweb.ro>
2013-08-07 09:20:35 +0300

    receive json response

--- elasticapi/eh.py
+++ elasticapi/eh.py
@@ -7,7 +7,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 def servers_info():
     url = '/'.join([API, 'drives/list'])
-    resp = requests.get(url, auth=(USER, SECRET_KEY))
+    resp = requests.get(
+               url,
+               auth=(USER, SECRET_KEY),
+               headers={'Accept': 'application/json'})
     return resp.text
 
 if __name__ == '__main__':
